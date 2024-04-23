2f1d5d082f745ed9f173e4f4369e67386ba0e66d
Pravin Shinde <shinde130@gmail.com>
2024-04-23 13:53:26 +0530

    initial commit

---
+++ src/main/java/com/dreamcoder/exceptionhandling/SpringGlobalExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.dreamcoder.exceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGlobalExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGlobalExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamcoder/exceptionhandling/SpringGlobalExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcoder.exceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class SpringGlobalExceptionHandlingApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
